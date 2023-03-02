9230c3290dd0f8a7bf6c73f43cbf2c007c23d3a9
asiedu-kev <asiedukevin050@gmail.com>
2023-03-02 23:50:41 +0100

    🎨 Add migrations and tweaks fro auth

---
+++ app/Http/Controllers/AccountRoleUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAccountRoleUserRequest;
+use App\Http\Requests\UpdateAccountRoleUserRequest;
+use App\Models\AccountRoleUser;
+
+class AccountRoleUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAccountRoleUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AccountRoleUser $accountRoleUser)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AccountRoleUser $accountRoleUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAccountRoleUserRequest $request, AccountRoleUser $accountRoleUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AccountRoleUser $accountRoleUser)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_03_02_223034_create_accounts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 255);
+            $table->string('timezone', 120)->nullable();
+            $table->string('country', 120)->nullable();
+            $table->char('locale', 2)->default('fr');
+            $table->uuid('owner_id');
+            $table->uuid('preference_id');
+            $table->enum('status', ['active', 'suspended', 'closed'])->default('active');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('owner_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table->foreign('preference_id')
+                ->references('id')
+                ->on('preferences')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
